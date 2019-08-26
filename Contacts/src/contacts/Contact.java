066f6191418e157bee2bf47022ce7a79b02e5482
Malik <mrjackson1@aggies.ncat.edu>
2019-08-26 17:25:34 -0400

    lab done

--- Contacts/src/contacts/Contact.java
+++ Contacts/src/contacts/Contact.java
@@ -10,6 +10,7 @@ package contacts;
  * @author CCannon
  */
 public class Contact {
+
     private String firstName;
     private String lastName;
     private String phoneNumber;
@@ -21,8 +22,65 @@ public class Contact {
         this.phoneNumber = phoneNumber;
         this.emailAddress = emailAddress;
     }
-    
-    public String toString(){
-        return firstName + " " + lastName  " " + phoneNumber + " " + emailAddress;
+
+    public String toString() {
+        return firstName + " " + lastName + " \n " + phoneNumber + "  \n " + emailAddress;
+    }
+
+    /**
+     * @return the firstName
+     */
+    public String getFirstName() {
+        return firstName;
+    }
+
+    /**
+     * @param firstName the firstName to set
+     */
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    /**
+     * @return the lastName
+     */
+    public String getLastName() {
+        return lastName;
+    }
+
+    /**
+     * @param lastName the lastName to set
+     */
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    /**
+     * @return the phoneNumber
+     */
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    /**
+     * @param phoneNumber the phoneNumber to set
+     */
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    /**
+     * @return the emailAddress
+     */
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    /**
+     * @param emailAddress the emailAddress to set
+     */
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
     }
 }
+
